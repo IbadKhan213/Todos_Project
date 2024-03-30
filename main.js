97631ccead4cac7335420e52cdbb30d89a5ff11b
M.Ibad khan <160883583+IbadKhan213@users.noreply.github.com>
2024-03-31 00:12:45 +0500

    i created a todo list

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    const list = await inquirer.prompt([
+        {
+            type: "input",
+            name: "ques1",
+            message: "What you want to add in your todos"
+        },
+        {
+            type: "confirm",
+            name: "ques2",
+            message: "ARE you sure you want to add more todo"
+        }
+    ]);
+    todos.push(list.ques1);
+    condition = list.ques2;
+    console.log(todos);
+}
+console.log("THANKS FOR LIST");
